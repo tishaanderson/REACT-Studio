14b3450f1d16392df9782602d74e036b69ca2596
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-05-09 21:09:38 -0500

    ADD PAGES FOLDER - Added Home.jsx along with content.
    - Added Portfolio.jsx, Resume.jsx, and Contact.jsx files but they are empty.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className='name' id='firstName'>Tisha <span id='lastName'>Anderson</span> </h2>   
+      {/* <img src="src\images\closingday2.png" alt="selfie of tisha anderson" /> */}
+         
+      <h3 className='title'>
+        Full-Stack Software Engineer <br />:: Junior Status :: 
+      </h3>
+
+      <p>
+      Hi, I'm Tisha Anderson, a junior software developer driven by the belief that technology can empower communities. Coming from a small town, I aim to simplify technology for everyday people and support small businesses. As the first woman in my family to graduate from college, I'm inspired to create generational wealth. Eager to learn and grow, I approach challenges with an open mind and commitment to excellence. My goal is to make a meaningful impact beyond the tech world, fostering trust and economic growth in local communities. Join me as we explore the intersection of technology and community, united in creating a more supportive global community.
+      </p>
+    </div>
+  );
+}
