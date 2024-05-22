47413f0660d04ffa11c395cb5eb3f3bfcda190f0
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-05-22 12:42:46 -0500

    UPDATED HOME PARAGRAPH CSS - Added background, text-shadow, color, and a media query for Home paragraph in all.css file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
-// import NameTitle from "../components/Title";
 
 function Home() {
   return (
-    <div>
-
-      {/* <NameTitle /> */}
-
-      <p>
-        Hi, I'm Tisha Anderson, a junior software developer driven by the belief that technology can empower communities. Coming from a small town, I aim to simplify technology for everyday people and support small businesses. As the first woman in my family to graduate from college, I'm inspired to create generational wealth. Eager to learn and grow, I approach challenges with an open mind and commitment to excellence. My goal is to make a meaningful impact beyond the tech world, fostering trust and economic growth in local communities. Join me as we explore the intersection of technology and community, united in creating a more supportive global community.
+    <div className="about-me">
+      <p className="about-me-p">
+        Hi, I'm Tisha Anderson, a junior software developer driven by the belief
+        that technology can empower communities. 
+      </p>
+      <p className="about-me-p">
+        Coming from a small town, I aim to simplify technology for everyday
+        people and support small businesses.
+      </p>
+      <p className="about-me-p">
+        As the first woman in my family to graduate from college, I'm inspired
+        to create generational wealth.
+      </p>
+      <p className="about-me-p">
+        Eager to learn and grow, I approach challenges with an open mind and
+        commitment to excellence. My goal is to make a meaningful impact beyond
+        the tech world, fostering trust and economic growth in local
+        communities. 
+      </p>
+      <p className="about-me-p">
+        Join me as we explore how technology can bring us together and create a more supportive global community. 😊 
       </p>
     </div>
   );
