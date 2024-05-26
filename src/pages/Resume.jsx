80ab3576fbf73713eed4496c09dab2bead61abed
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-05-25 20:25:16 -0500

    UPDATED RESUME PAGE TO BE DYNAMIC - Resume sections are entered dynamically on the page.
    - CSS has been updated.
    
    - TEXT ANIMATIONS NEED TO BE ADJUSTEDgit add .

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,124 +1,169 @@
 // import '../styles/Resume.css';
 
-
 export default function Resume() {
+  const education = [
+    {
+      degree: "Full-Stack Web Development Boot Camp Certificate",
+      school: "Southern Methodist University",
+    },
+    {
+      degree: "Bachelor of Science (B.S.)",
+      school: "University of North Texas",
+    },
+  ];
+
+  const experience = [
+    {
+      years: "2020-2023",
+      title: "Quality Assurace Lead",
+      company: "Custom Ink",
+      responsibilities: [
+        "Responsible for collaborating closely with product teams to align quality assurance processes with development goals and timelines.",
+        "Led and guided the quality assurance team, fostering collaboration and teamwork to ensure adherence to company standards and objectives.",
+        "Effectively delegated tasks within the QA team for the successful implementation of new features and functionalities.",
+        "Developed and implemented SOPs to enhance efficiency and consistency in QA activities, optimizing performance and productivity.",
+        "Identified and addressed areas for process improvement, driving continuous enhancement across departments.",
+        "Provided training and mentorship to QA team members, facilitating skill development and knowledge sharing.",
+        "Prioritized customer satisfaction by maintaining a qualityfocused approach throughout product development.",
+        "Proactively identified and mitigated risks to product quality, safety, and compliance, ensuring adherence to industry standards and best practices while optimizing performance.",
+      ],
+      key_accomplishments: [
+        "Spearheaded a quality improvement initiative within an 8-member team, elevating quality rating from levels 1-3 to an exceptional level of 6-8 (with 8 representing the highest standard).",
+        "Orchestrated a transformation within the shipping department, increasing output by nearly 50% compared to previous levels.",
+        "Fostered a culture of teamwork and collaboration, resulting in a streamlined operational process with mistakes decreasing to less than 10% of previous levels.",
+        "Achieved outstanding results in both quality assurance and shipping departments, setting new standards for operational excellence.",
+      ],
+    },
+    {
+      years: "2016-2020",
+      title: "Bartender",
+      company: "Buffalo Wild Wings",
+      responsibilities: [
+        "Provided exceptional customer service by courteously engaging with patrons and accurately taking orders.",
+        "Expertly mixed and served drinks according to standard recipes and customer preferences.",
+        "Processed payments accurately using cash registers or POS systems, maintaining cash draw integrity.",
+        "Actively promoted specials and upsold premium drinks to enhance sales and customer satisfaction.",
+      ],
+    },
+    {
+      years: "2016",
+      title: "Front Desk Associate",
+      company: "FYZICAL Therapy and Balance Centers",
+      responsibilities: [
+        "Effectively communicated with clients via phone, email, and in-person interactions.",
+        "Demonstrated strong multitasking abilities to ensure smooth front desk operations.",
+        "Exhibited excellent organizational skills in managing daily front desk responsibilities.",
+      ],
+    },
+  ];
+
+  const skills = {
+    fields: [
+      "Full-Stack Web Development",
+      "Quality Assurance",
+      "Applied Behavior Analysis",
+    ],
+    programmingLanguages: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "OOP",
+      "SQL",
+      "NoSQL",
+      "MySQL",
+      "Bootstrap",
+      "MongoDB",
+      "jQuery",
+      "Express.js",
+      "GraphQL",
+    ],
+    coding: ["REACT", "Node.js", "MVC", "Handlebars.js"],
+    personalStrengths: [
+      "Problem Solving",
+      "Strategic Decision Making",
+      "Reinforcement Strategies",
+      "Organization and Time Management",
+      "SOPs",
+      "Streamlining Processes",
+      "Cost-Savings Strategies",
+      "Developing Quality Standards",
+      "Strong Analytical Abilities",
+      "Task Prioritization",
+    ],
+  };
+
   return (
-    <div className='resume'>
-
-      <div className='resume-card' id='education-section'>
-        <h3 className='resume-section'>Education</h3>
-        <h6>Bachelor of Science (B.S.) </h6>
-        <p>University of North Texas</p>
-        <h6>Full-Stack Coding Bootcamp (Certification) </h6>
-        <p>Southern Methodist University</p>
+    <div className="resume">
+      <div className="resume-card" id="education-section">
+        <h3 className="resume-section">Education</h3>
+        {education.map((edu, index) => (
+          <div key={index}>
+            <h6>{edu.degree}</h6>
+            <p>{edu.school}</p>
+          </div>
+        ))}
       </div>
 
-      <div className='resume-card' id='experience-section'>
-        <h3 className='resume-section'>Experience</h3>
-
-        <p className='job-years'>2020-2023</p>
-        <h6 className='job-title'>Quality Assurance Lead <i className='company-name'>Custom Ink</i></h6>
-        <ul className='job-responsibilities'>
-          <ul className='job-list'>Provide leadership and guidance to the quality assurance team, ensuring
-            alignment with organizational goals and objectives.</ul>
-          <ul className='job-list'>Assign tasks and delegate responsibilities effectively to ensure the
-            team’s success.</ul>
-          <ul className='job-list'>Develop and implement standard operating procedures (SOPs) to
-            ensure consistent and efficient processes.</ul>
-          <ul className='job-list'>Identify areas for process improvement and collaborate with other
-            departments to properly implement solutions.</ul>
-          <ul className='job-list'>Train and mentor quality assurance team members on quality standards,
-            processes, and protocols.
-          </ul>
-          <ul className='job-list'>Maintain a customer-centric approach by prioritizing quality and
-            ensuring customer satisfaction.</ul>
-          <ul className='job-list'>Identify potential risks to quality and develop strategies to mitigate them.</ul>
-          <ul className='job-list' id='last-job-list'>Proactively address issues that may impact product quality, safety or
-            compliance.
-          </ul>
-        </ul>
-
-        <p className='job-years'>2016-2020</p>
-        <h6 className='job-title'>Bartender <i className='company-name'>Buffalo Wild Wings</i></h6>
-        <ul className='job-responsibilities'>
-          <ul className='job-list'>Provide exceptional customer service by taking orders accurately and
-            engaging with patrons courteously.</ul>
-          <ul className='job-list'>Mix and serve a variety of drinks according to standard recipes and
-            customer preferences.</ul>
-          <ul className='job-list'>Process payments accurately using cash registers or POS systems and
-            maintain cash drawer integrity.
-          </ul>
-          <ul className='job-list' id='last-job-list'>Promote specials and upsell premium drinks to enhance sales and
-            customer experience.
-          </ul>
-        </ul>
-
-        <p className='job-years'>Summer 2016</p>
-        <h6 className='job-title'>Front Desk Associate <i className='company-name'>FYZICAL Therapy and Balance Centers</i></h6>
-        <ul className='job-responsibilities' id='summer-job'>
-          <ul className='job-list'>Communicate effectively via phone, email, and in-person with clients.</ul>
-          <ul className='job-list'>Excel in multitasking clerical tasks</ul>
-          <ul className='job-list' id='last-job-list'>Demonstrate excellent organizational skills
-          </ul>
-        </ul>
+      <div className="resume-card" id="experience-section">
+        <h3 className="resume-section">Experience</h3>
+        {experience.map((job, index) => (
+          <div key={index}>
+            <p className="job-years">{job.years}</p>
+            <h6 className="job-title">
+              {job.title} <i className="company-name">{job.company}</i>
+            </h6>
+            <ul className="job-responsibilities">
+              {job.responsibilities.map((task, idx) => (
+                <li key={idx} className="job-list">
+                  {task}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
 
-      <div className='resume-card' id='skills-section'>
-        <h3 className='resume-section'>Skills</h3>
-        <div className='skills-container'>
-
-          <ul className='skills'>
-            <h5 className='skill-type'>Fields:</h5>
-
-            <li className='skill-list'>Full-Stack Development</li>
-            <li className='skill-list'>Quality Assurance</li>
-            <li className='skill-list'>Applied Behavior Analysis</li>
+      <div className="resume-card" id="skills-section">
+        <h3 className="resume-section">Skills</h3>
+        <div className="skills-container">
+          <ul className="skills">
+            <h5 className="skill-type">Fields:</h5>
+            {skills.fields.map((field, index) => (
+              <li key={index} className="skill-list">
+                {field}
+              </li>
+            ))}
           </ul>
-
-
-
-          <ul className='skills'>
-            <h5 className='skill-type'>Programming Languages:</h5>
-
-            <li className='skill-list'>HTML</li>
-            <li className='skill-list'>CSS</li>
-            <li className='skill-list'>JavaScript</li>
-            <li className='skill-list'>SQL</li>
-            <li className='skill-list'>NoSQL</li>
-
+          <ul className="skills">
+            <h5 className="skill-type">Programming Languages:</h5>
+            {skills.programmingLanguages.map((language, index) => (
+              <li key={index} className="skill-list">
+                {language}
+              </li>
+            ))}
           </ul>
-
-
-
-          <ul className='skills'>
-            <h5 className='skill-type'>Coding:</h5>
-
-            <li className='skill-list'>REACT</li>
-            <li className='skill-list'>Node</li>
+          <ul className="skills">
+            <h5 className="skill-type">Coding:</h5>
+            {skills.coding.map((code, index) => (
+              <li key={index} className="skill-list">
+                {code}
+              </li>
+            ))}
           </ul>
         </div>
 
         <div>
-          <h5 className='skill-type' id='personal-skills'>Personal Strengths:</h5>
-        </div>
-
-        <div className='strengths'>
-          <ul className='skills'>
-            
-
-            <li className='skill-list'>Problem Solving</li>
-            <li className='skill-list'>Strategic Decision Making</li>
-            <li className='skill-list'>Reinforcement Strategies</li>
-            <li className='skill-list'>Organization and Time Management</li>
-          </ul>
-          <ul className='skills' id=''>
-            <li className='skill-list'>Knowledgeable on creating SOPs, effective streamline processes and cost-savings strategies</li>
-            <li className='skill-list'>Strong Analytical Abilities</li>
-            <li className='skill-list'>Deleloping Quality Standards</li>
-            <li className='skill-list'>Task Prioritization</li>
+          <h5 className="skill-type" id="personal-skills">
+            Personal Strengths:
+          </h5>
+          <ul className="skills">
+            {skills.personalStrengths.map((strength, index) => (
+              <li key={index} className="skill-list">
+                {strength}
+              </li>
+            ))}
           </ul>
         </div>
-
       </div>
     </div>
   );
